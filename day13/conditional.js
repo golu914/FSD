717a9236aa1198e280157fd7ded768d84bf34d71
golu914 <shantanupratap58@gmail.com>
2025-09-10 21:16:12 +0530

    fsd

---
+++ day13/conditional.js
@@ -0,0 +1,35 @@
+/*
+let age=17;
+if(age>=18){
+    console.log("you are eligible to vote");
+}
+else{
+    console.log("You are not eligible to vote");
+}
+    */
+
+   let marks = [38, 27, 30, 25, 80, "hello", true, "A", null, "+-*"]
+ console.log(marks[0])
+ console.log(marks[5])
+ console.log(marks.length)
+
+for (let i = 0; i < marks.length; i++) {
+    console.log(marks[i])
+}
+
+/*
+let marks = [38, 27, 30, 25, 80, "hello", true, "A", null, "+-*"]
+// console.log(marks[0])
+// console.log(marks[5])
+// console.log(marks.length)
+/*
+for (let i = 0; i < marks.length; i++) {
+    console.log(marks[i])
+}
+*/
+/*
+for(let val of marks){
+    console.log(typeof val)
+    console.log(val)
+}
+    */
